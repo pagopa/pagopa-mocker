5827ea1e9780430db46d0df9ee51f7646ded2bc1
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-02-15 16:12:43 +0100

    fix: added CORS headers in /info API

--- src/main/java/it/gov/pagopa/mocker/config/WebConf.java
+++ src/main/java/it/gov/pagopa/mocker/config/WebConf.java
@@ -4,12 +4,9 @@ import javax.servlet.http.HttpServlet;
 
 import it.gov.pagopa.mocker.controller.ProxyServlet;
 import it.gov.pagopa.mocker.util.Constants;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Optional;
 
 @Configuration
 public class WebConf {
