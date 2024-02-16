d17c6bb02be931425b2d31ff8b4f023bdca77563
Andrea De Rinaldis <andrea.derinaldis@pagopa.it>
2024-02-16 08:15:51 +0100

    feat: added feature flag on caching for mock responses

--- src/main/java/it/gov/pagopa/mocker/entity/MockResponseEntity.java
+++ src/main/java/it/gov/pagopa/mocker/entity/MockResponseEntity.java
@@ -19,6 +19,8 @@ public class MockResponseEntity implements Serializable {
 
     private int status;
 
+    private Boolean isCacheable;
+
     private List<String> parameters;
 
     private List<ResponseHeaderEntity> headers;
